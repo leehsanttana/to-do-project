1f1c2b5019bb6eb211d58efef3c825b4dd145013
leehsanttana <leonamsanttana@gmail.com>
2021-03-15 21:58:45 -0300

    criando um projeto de to-dos. foram feito as regras para que não se criasse um to-do até que fosse digitado um to-do, de editar apenas uma vez, de salvar o que foi modificiado e de remover um to-do

---
+++ js/script.js
@@ -0,0 +1,67 @@
+const input = document.querySelector('[data-todo="input"');
+const addBtn = document.querySelector('[data-todo="add-btn"');
+const todoContainer = document.querySelector('[data-todo="container"');
+
+const ulElement = document.createElement('ul');
+todoContainer.appendChild(ulElement);
+
+function createTodo() {
+  if (input.value !== '') {
+    const liElement = document.createElement('li');
+    ulElement.appendChild(liElement);
+
+    const toDo = document.createElement('p');
+    toDo.innerText = input.value;
+
+    const liEdit = document.createElement('button');
+    liEdit.innerText = 'Editar';
+    const liRemove = document.createElement('button');
+    liRemove.innerText = 'Remover';
+
+    liElement.appendChild(toDo);
+    liElement.appendChild(liEdit);
+    liElement.appendChild(liRemove);
+
+    let active = false;
+
+    function editTodo() {
+      if (!active) {
+        const divEditTodo = document.createElement('div');
+        liElement.appendChild(divEditTodo);
+
+        const liInput = document.createElement('input');
+        liInput.setAttribute('type', 'text');
+        liInput.value = toDo.innerText;
+        divEditTodo.appendChild(liInput);
+
+        const liSave = document.createElement('button');
+        liSave.innerText = 'Salvar';
+        liInput.after(liSave);
+
+        active = true;
+
+        function saveTodo() {
+          toDo.innerText = liInput.value;
+          console.log('A alteração foi salva!');
+
+          active = false;
+
+          setTimeout(() => {
+            divEditTodo.style.display = 'none';
+          }, 1000);
+        }
+
+        liSave.addEventListener('click', saveTodo);
+      }
+    }
+
+    function removeTodo() {
+      liElement.remove();
+    }
+
+    liEdit.addEventListener('click', editTodo);
+    liRemove.addEventListener('click', removeTodo);
+  }
+}
+
+addBtn.addEventListener('click', createTodo);
